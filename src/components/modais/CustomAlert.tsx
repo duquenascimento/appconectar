c2a20ec7b36ced22f0a8c1e7bfee1ba50a70e41b
Arthur-Luiz19 <arthur.luiz11@hotmail.com>
2025-04-01 09:13:16 -0300

    [rollback 01-04-2025] feature: ajustes nos nomes dos campos do fieldLabels

--- src/components/modais/CustomAlert.tsx
+++ src/components/modais/CustomAlert.tsx
@@ -1,70 +1,76 @@
-import React from 'react';
-import { Modal, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from "react";
+import { Modal, View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 interface CustomAlertProps {
-    visible: boolean;
-    title: string;
-    message: string;
-    onConfirm: () => void;
+  visible: boolean;
+  title: string;
+  message: string;
+  onConfirm: () => void;
 }
 
-const CustomAlert: React.FC<CustomAlertProps> = ({ visible, title, message, onConfirm }) => {
-    return (
-        <Modal
-            transparent={true}
-            visible={visible}
-            animationType="fade"
-            onRequestClose={onConfirm}
-        >
-            <View style={styles.overlay}>
-                <View style={styles.alertContainer}>
-                    <Text style={styles.title}>{title}</Text>
-                    <Text style={styles.message}>{message}</Text>
-                    <TouchableOpacity style={styles.button} onPress={onConfirm}>
-                        <Text style={styles.buttonText}>OK</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </Modal>
-    );
+const CustomAlert: React.FC<CustomAlertProps> = ({
+  visible,
+  title,
+  message,
+  onConfirm,
+}) => {
+  return (
+    <Modal
+      transparent={true}
+      visible={visible}
+      animationType="fade"
+      onRequestClose={onConfirm}
+    >
+      <View style={styles.overlay}>
+        <View style={styles.alertContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.message}>{message}</Text>
+          <TouchableOpacity style={styles.button} onPress={onConfirm}>
+            <Text style={styles.buttonText}>OK</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
 };
 
 const styles = StyleSheet.create({
-    overlay: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        zIndex: 9999, 
-    },
-    alertContainer: {
-        width: '80%',
-        backgroundColor: 'white',
-        borderRadius: 10,
-        padding: 20,
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    message: {
-        fontSize: 14,
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    button: {
-        backgroundColor: '#04BF7B',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 14,
-        fontWeight: 'bold',
-    },
+  overlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    zIndex: 9999,
+  },
+  alertContainer: {
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  message: {
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 20,
+    color: "red",
+  },
+  button: {
+    backgroundColor: "#04BF7B",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
 });
 
 export default CustomAlert;
