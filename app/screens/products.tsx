0655d6bc85c506d0e0baf37dd1a3a890f09fd706
Arthur-Luiz19 <arthur.luiz11@hotmail.com>
2025-04-10 15:28:27 -0300

    style: ajusta a largura e alinhamento do dropdown

--- app/screens/products.tsx
+++ app/screens/products.tsx
@@ -1706,6 +1706,10 @@ export function Products({ navigation }: HomeScreenProps) {
         }
         listMode="SCROLLVIEW"
         dropDownDirection="BOTTOM"
+        dropDownContainerStyle={{ 
+          width: Platform.OS === "web" ? "68%" : "92%",
+          alignSelf: "center"
+         }}
         style={{
           width: Platform.OS === "web" ? "68%" : "92%",
           alignSelf: 'center',
