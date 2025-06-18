a834cb138175af2ade822d441a7757c91f50c840
Arthur Luiz <arthur.luiz11@hotmail.com>
2025-06-18 19:35:43 -0300

    fix: corrige o bug do botão carrinho não aparecer

--- app/utils/FlatList_VirtualizeList/VirtualizeList_Products.tsx
+++ app/utils/FlatList_VirtualizeList/VirtualizeList_Products.tsx
@@ -1,31 +1,19 @@
 // utils/CustomVirtualizedList.tsx
-import React from 'react';
-import { VirtualizedList, View } from 'react-native';
+import React from 'react'
+import { VirtualizedList, View, NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
 
 type Props = {
-  data: any[];
-  renderItem: ({ item }: any) => JSX.Element;
-  keyExtractor: (item: any) => string;
-  listRef?: React.RefObject<VirtualizedList<any>>;
-};
+  data: any[]
+  renderItem: ({ item }: any) => JSX.Element
+  keyExtractor: (item: any) => string
+  listRef?: React.RefObject<VirtualizedList<any>>
+  onScroll?: (event: NativeSyntheticEvent<NativeScrollEvent>) => void
+  onMomentumScrollBegin?: () => void
+  onMomentumScrollEnd?: () => void
+}
 
-const CustomVirtualizedList = ({ data, renderItem, keyExtractor, listRef }: Props) => {
-  return (
-    <VirtualizedList
-      ref={listRef}
-      data={data}
-      getItem={(data, index) => data[index]}
-      getItemCount={(data) => data.length}
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
-      initialNumToRender={10}
-      windowSize={5}
-      keyboardShouldPersistTaps="handled"
-      keyboardDismissMode="on-drag"
-      style={{ flex: 1 }}
-      ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
-    />
-  );
-};
+const CustomVirtualizedList = ({ data, renderItem, keyExtractor, listRef, onScroll, onMomentumScrollBegin, onMomentumScrollEnd }: Props) => {
+  return <VirtualizedList ref={listRef} data={data} getItem={(data, index) => data[index]} getItemCount={(data) => data.length} keyExtractor={keyExtractor} renderItem={renderItem} initialNumToRender={10} windowSize={5} keyboardShouldPersistTaps="handled" keyboardDismissMode="on-drag" style={{ flex: 1 }} onScroll={onScroll} onMomentumScrollBegin={onMomentumScrollBegin} onMomentumScrollEnd={onMomentumScrollEnd} ItemSeparatorComponent={() => <View style={{ height: 8 }} />} />
+}
 
-export default CustomVirtualizedList;
+export default CustomVirtualizedList
