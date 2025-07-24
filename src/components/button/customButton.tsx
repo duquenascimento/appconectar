c6a5e8c3bc7dea2cbbb0b0d8027c634ca9737c89
Luiz Silva <luiz.qos@gmail.com>
2025-07-24 15:00:57 -0300

    Adicionado width para button

--- src/components/button/customButton.tsx
+++ src/components/button/customButton.tsx
@@ -7,12 +7,13 @@ interface ButtonComponentProps {
   onPress: (event: GestureResponderEvent) => void
   backgroundColor?: string
   textColor?: string
+  width?: string
 }
 
-const CustomButton: React.FC<ButtonComponentProps> = ({ title, onPress, backgroundColor = '#04BF7B', textColor = 'white' }) => {
+const CustomButton: React.FC<ButtonComponentProps> = ({ title, onPress, backgroundColor = '#04BF7B', textColor = 'white', width }) => {
   return (
     <Button
-      backgroundColor={backgroundColor}
+      backgroundColor={backgroundColor} 
       onPress={onPress}
       alignItems="center"
       justifyContent="center"
@@ -21,7 +22,7 @@ const CustomButton: React.FC<ButtonComponentProps> = ({ title, onPress, backgrou
         background: backgroundColor,
         opacity: 0.9
       }}
-      width={Platform.OS === 'web' ? '70%' : '92%'}
+      width={width || (Platform.OS === 'web' ? '70%' : '92%')}
       alignSelf="center"
     >
       <Text fontWeight="500" fontSize={16} color={textColor}>
