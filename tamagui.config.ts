41cc7ccf7e21027f5becada7ecc796c7d1275f04
Davi Duque do Nascimento <daviduquedonascimento@Davis-MacBook-Pro.local>
2025-09-12 16:21:55 -0300

    asjuste para iphone

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,7 +1,25 @@
 import { config } from '@tamagui/config/v3'
 import { createTamagui } from 'tamagui'
+import { themes, tokens } from '@tamagui/themes'
 
-export const tamaguiConfig = createTamagui(config)
+const tamaguiConfig = createTamagui({
+  themes,
+  tokens,
+  components: {
+    Dialog: true,
+    Portal: true, // 👈 Essencial para o modal funcionar!
+    Sheet: true,
+    Button: true,
+    Text: true,
+    Input: true,
+    XStack: true,
+    YStack: true,
+    Image: true,
+    View: true,
+    Stack: true,
+    Adapt: true,
+  }
+})
 
 export default tamaguiConfig
 
