2e09ecfd34c32407011dc4df092bf40bafc388cf
Arthur Luiz <arthur.luiz11@hotmail.com>
2025-09-26 18:15:22 -0300

    feat: Chama rota de carrinho abandonado

--- src/components/cartButton.tsx
+++ src/components/cartButton.tsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useRef } from 'react'
 import { Platform, TouchableOpacity } from 'react-native'
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming
-} from 'react-native-reanimated'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 import { View, Text } from 'tamagui'
 import { Product } from '@/app/products'
 import Icons from '@expo/vector-icons/Ionicons'
+import { AbandonedCartWatcher } from '../utils/abandonedCart'
+import { getStorage } from '../utils/utils'
 
 type Props = {
   cartSize: number
   isScrolling: boolean
   visibleProducts: Product[]
+  selectedRestaurant: string | null
   onPress: () => void
 }
 
-export const CartButton: React.FC<Props> = ({
-  cartSize,
-  isScrolling,
-  visibleProducts,
-  onPress
-}) => {
+interface Restaurant {
+  id: string
+  externalId: string
+  name: string
+  user: string
+}
+
+export const CartButton: React.FC<Props> = ({ cartSize, isScrolling, visibleProducts, selectedRestaurant, onPress }) => {
   const opacity = useSharedValue(0)
   const translateY = useSharedValue(50)
   const hideTimeout = useRef<NodeJS.Timeout | null>(null)
@@ -61,8 +62,34 @@ export const CartButton: React.FC<Props> = ({
     pointerEvents: opacity.value === 1 ? 'auto' : 'none'
   }))
 
+  const loadAndRenderWatcher = async () => {
+    if (!selectedRestaurant || cartSize <= 0) return null
+
+    try {
+      const data = await getStorage('selectedRestaurant')
+      if (!data) return null
+      const parsed = JSON.parse(data)
+      const restaurant: Restaurant = parsed.restaurant
+      if (restaurant.externalId !== selectedRestaurant) return null
+
+      return (
+        <AbandonedCartWatcher
+          cartSize={cartSize}
+          selectedRestaurant={{ restaurant }}
+        />
+      )
+    } catch (error) {
+      console.error('Erro ao carregar restaurante do AsyncStorage:', error)
+      return null
+    }
+  }
+  const WatcherElement = loadAndRenderWatcher()
+
+
   if (Platform.OS === 'web') {
     return (
+      <>
+        {WatcherElement}
       <div
         style={{
           position: 'absolute',
@@ -131,10 +158,13 @@ export const CartButton: React.FC<Props> = ({
           </div>
         </button>
       </div>
+      </>
     )
   }
 
   return (
+    <>
+        {WatcherElement}
     <Animated.View
       style={[
         {
@@ -150,38 +180,11 @@ export const CartButton: React.FC<Props> = ({
       ]}
       pointerEvents="box-none"
     >
-      <TouchableOpacity
-        activeOpacity={0.9}
-        onPress={cartSize > 0 ? onPress : undefined}
-        disabled={cartSize <= 0}
-      >
-        <View
-          backgroundColor="#FFA500"
-          width={160}
-          height={45}
-          borderRadius={24}
-          paddingHorizontal={16}
-          paddingVertical={8}
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="center"
-          pointerEvents="auto"
-        >
+      <TouchableOpacity activeOpacity={0.9} onPress={cartSize > 0 ? onPress : undefined} disabled={cartSize <= 0}>
+        <View backgroundColor="#FFA500" width={160} height={45} borderRadius={24} paddingHorizontal={16} paddingVertical={8} flexDirection="row" alignItems="center" justifyContent="center" pointerEvents="auto">
           <View>
             <Icons size={25} color="white" name="cart" />
-            <View
-              position="absolute"
-              bottom={-1}
-              right={-5}
-              backgroundColor="white"
-              borderRadius={10}
-              width={15}
-              height={15}
-              alignItems="center"
-              justifyContent="center"
-              borderColor="#FFA500"
-              borderWidth={1}
-            >
+            <View position="absolute" bottom={-1} right={-5} backgroundColor="white" borderRadius={10} width={15} height={15} alignItems="center" justifyContent="center" borderColor="#FFA500" borderWidth={1}>
               <Text fontSize={9} color="#FFA500">
                 {cartSize}
               </Text>
@@ -193,5 +196,6 @@ export const CartButton: React.FC<Props> = ({
         </View>
       </TouchableOpacity>
     </Animated.View>
+    </>
   )
 }
