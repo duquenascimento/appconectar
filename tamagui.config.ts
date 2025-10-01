5239dd84199e39b8a30af2366217755719e037a0
Arthur Luiz <arthur.luiz11@hotmail.com>
2025-10-01 17:20:50 -0300

    fix: ajusta as configurações do tamagui para diferentes sistemas operacionais

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,30 +1,55 @@
+import { Platform } from 'react-native'
 import { config } from '@tamagui/config/v3'
 import { createTamagui } from 'tamagui'
 import { themes, tokens } from '@tamagui/themes'
 
-const tamaguiConfig = createTamagui({
-  ...config,
-  themes,
-  tokens,
-  components: {
-    Portal: true,
-    ...((config as any).components || {}),
-    Dialog: true,
-    Sheet: true,
-    Button: false,
-    Text: false,
-    Input: true,
-    XStack: true,
-    YStack: true,
-    Image: false,
-    View: false,
-    Stack: false,
-    Adapt: true
-  }
-})
+const tamaguiOptions =
+  Platform.OS === 'ios'
+    ? {
+        themes,
+        tokens,
+        components: {
+          Portal: true,
+          ...((config as any).components || {}),
+          Dialog: true,
+          Sheet: true,
+          Button: false,
+          Text: false,
+          Input: true,
+          XStack: true,
+          YStack: true,
+          Image: false,
+          View: false,
+          Stack: false,
+          Adapt: true
+        }
+      }
+    : {
+        ...config,
+        components: {
+          Portal: true,
+          ...((config as any).components || {})
+          /*
+          Dialog: true,
+          Sheet: true,
+          Button: false,
+          Text: false,
+          Input: true,
+          XStack: true,
+          YStack: true,
+          Image: false,
+          View: false,
+          Stack: false,
+          Adapt: true,
+          */
+        }
+      }
+
+const tamaguiConfig = createTamagui(tamaguiOptions)
 
 export default tamaguiConfig
 export type Conf = typeof tamaguiConfig
+
 declare module 'tamagui' {
   interface TamaguiCustomConfig extends Conf {}
 }
