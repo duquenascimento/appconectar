4a743ad329f2f5baf5b9f06881a5c06b1b462e64
Arthur Luiz <arthur.luiz11@hotmail.com>
2025-10-01 14:24:58 -0300

    feat: Melhora as configurações do eslint baseado no airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,52 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-hooks', 'react-native', '@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'max-len': ['warn', { code: 100, ignoreComments: true }],
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
+
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-raw-text': 'off',
+    'react-native/no-color-literals': 'off',
+
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
