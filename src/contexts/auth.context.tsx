85ad856ff0e748decd82d29f24fa5edcee98f147
felieemidio <felipe.dev.emidio@gmail.com>
2025-12-29 14:34:15 -0300

    refactor: impove performance AuthContext

--- src/contexts/auth.context.tsx
+++ src/contexts/auth.context.tsx
@@ -1,5 +1,13 @@
-import { createContext, ReactNode, useCallback, useContext, useEffect, useMemo, useState } from "react";
-import { deleteToken, getToken, setToken } from "../utils/utils";
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import { deleteToken, getToken, setToken } from '../utils/utils';
 
 interface AuthContextProps {
   authToken: string | null;
@@ -10,50 +18,50 @@ interface AuthContextProps {
 const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-    const [authToken, setAuthToken] = useState<string | null>(null);
+  const [authToken, setAuthToken] = useState<string | null>(null);
 
-    const saveAuthToken = useCallback(async (token: string) => {
-        try {
-            await setToken(token);
-            setAuthToken(token);
-        } catch (error) {
-            console.error('Erro ao salvar token:', error);
+  const saveAuthToken = useCallback(async (token: string) => {
+    try {
+      await setToken(token);
+      setAuthToken(token);
+    } catch (error) {
+      console.error('Erro ao salvar token:', error);
+    }
+  }, []);
+
+  const deleteAuthToken = useCallback(async () => {
+    try {
+      await deleteToken();
+      setAuthToken(null);
+    } catch (error) {
+      console.error('Erro ao deletar token:', error);
+    }
+  }, []);
+
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const token = await getToken();
+        if (token) {
+          setAuthToken(token);
         }
-    }, []);
+      } catch (error) {
+        console.error('Erro ao buscar o token:', error);
+      }
+    };
+    initialize();
+  }, []);
 
-    const deleteAuthToken = useCallback(async () => {
-        try {
-            await deleteToken();
-            setAuthToken(null);
-        } catch (error) {
-            console.error('Erro ao deletar token:', error);
-        }
-    }, []);
+  const value = useMemo(
+    () => ({
+      authToken,
+      saveAuthToken,
+      deleteAuthToken,
+    }),
+    [authToken, saveAuthToken, deleteAuthToken],
+  );
 
-    useEffect(() => {
-        const initialize = async () => {
-            try {
-            const token = await getToken();
-            if (token) {
-                setAuthToken(token);
-            }
-            } catch (error) {
-                console.error('Erro ao buscar o token:', error);
-            }
-        };
-        initialize();
-    }, []);
-
-    const value = useMemo(
-        () => ({
-            authToken,
-            saveAuthToken,
-            deleteAuthToken,
-        }),
-        [authToken, saveAuthToken, deleteAuthToken],
-      );
-
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export const useAuthContext = () => useContext(AuthContext);
