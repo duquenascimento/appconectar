6beffaa565a05da827c8fbaa9ce5f15a9c9ad480
felipeemidio <felipe.dev.emidio@gmail.com>
2026-02-06 18:03:01 -0300

    fix: pedido duplicado

--- src/utils/errorUtils.ts
+++ src/utils/errorUtils.ts
@@ -1,3 +1,5 @@
+import { AxiosError } from "axios";
+
 export function handleHttpException(error: any): Error {
   let message: string = 'Houve um erro ao processar a solicitação.';
   if (error.response) {
@@ -7,4 +9,20 @@ export function handleHttpException(error: any): Error {
   }
 
   return new Error(message);
+}
+
+export function extractErrorMessage(error: any): string {
+  let message: string = 'Houve um erro ao processar a solicitação.';
+
+  if(error instanceof AxiosError) {
+    if (error.response) {
+      message = error.response.data.msg ?? error.response.data.message;
+    } else {
+      message = error.message
+    }
+  } else if(error instanceof Error) {
+    message = error.message;
+  }
+
+  return message;
 }
