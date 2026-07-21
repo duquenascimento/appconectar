56a53cf2422d67cc103248cd7774eabd6aabb3b0
vitorva6 <vitorandrade@ecomp.uefs.br>
2026-07-21 12:45:37 -0300

    feat: adiciona ordenação seguindo critérios de favorito, grande quantidade e alfabético

--- src/types/productTypes.ts
+++ src/types/productTypes.ts
@@ -1,27 +1,28 @@
 export type Product = {
-    name: string
-    orderUnit: string
-    quotationUnit: string
-    convertedWeight: number
-    class: string
-    sku: string
-    id: string
-    active: boolean
-    createdBy: string
-    createdAt: string
-    changedBy: string
-    updatedAt: string
-    image: string[]
-    favorite?: boolean
-    mediumWeight: number
-    firstUnit: number
-    secondUnit: number
-    thirdUnit: number
-    obs: string
-}
+  name: string;
+  orderUnit: string;
+  quotationUnit: string;
+  convertedWeight: number;
+  class: string;
+  sku: string;
+  id: string;
+  active: boolean;
+  createdBy: string;
+  createdAt: string;
+  changedBy: string;
+  updatedAt: string;
+  image: string[];
+  favorite?: boolean;
+  mediumWeight: number;
+  firstUnit: number;
+  secondUnit: number;
+  thirdUnit: number;
+  obs: string;
+  isLargeQuantity?: boolean;
+};
 
 export type ProductClass = {
-    id: string
-    nome: string
-    ativo: boolean
-}
+  id: string;
+  nome: string;
+  ativo: boolean;
+};
